432c26e5c613eb58940a04de35d0639a42332cfb
Tomas Cahue <tomas.cahue@gmail.com>
2023-05-04 16:51:59 -0300

    Vida Nomade. Primeros maquetados

---
+++ Maquetado Salud Nomade Bootstrap/assets/Javascript/js.js
@@ -0,0 +1,12 @@
+/* "use strict"
+
+var mediaquery = window.matchMedia("(max-width: 1023px)");
+function funcion() {
+  var div = document.querySelector(".grande");
+  if (mediaquery.matches) {
+
+  } else {
+    div.classList.add("column-reverse");
+  }
+}
+mediaquery.addListener(funcion) */
